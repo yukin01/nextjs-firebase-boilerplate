f674d37b77d3311c2e2b70ca4392901916f76dad
yukin01 <bump1105@gmail.com>
2019-07-14 11:22:14 +0900

    make minimal configuration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from 'firebase-functions'
-import next from 'next'
 import * as path from 'path'
+import next from 'next'
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, conf: { distDir: `${path.relative(process.cwd(), __dirname)}/app` } })
+const distDir = `${path.relative(process.cwd(), __dirname)}/app`
+const app = next({ dev, conf: { distDir } })
 const handle = app.getRequestHandler()
 
 export const nextApp = functions.https.onRequest((req, res) => {
